ec24ca991d2dee4b21307dbbebc1c3e95471a9b5
chernyakma <mariia.cherniak@testpro.io>
2023-03-28 20:29:09 -0400

    HomeWork_18

---
+++ src/test/java/SongsTests.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SongsTests extends BaseTest{
+    @Test
+    public void PlaySong() throws InterruptedException {
+
+        login("chernyakma75@gmail.com","te$t$tudent" );
+
+        WebElement hover = driver.findElement(By.cssSelector("[title='Play or resume']"));
+        new Actions(driver)
+               .moveToElement(hover)
+               .perform();
+        Thread.sleep(5000);
+        hover.click();
+        WebElement pause=driver.findElement(By.cssSelector("[class='pause']"));
+        Assert.assertTrue(pause.isDisplayed());
+    }
+}
