478e7c0d64e51aedd0c20029c6e9a676652e4a4a
chernyakma <mariia.cherniak@testpro.io>
2023-04-02 17:14:09 -0400

    Update PlayListTests.java

--- src/test/java/PlayListTests.java
+++ src/test/java/PlayListTests.java
@@ -1,31 +1,22 @@
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PlayListTests extends BaseTest{
+public class PlaylistTests extends BaseTest{
     @Test
-    public void addSongToPlaylist()throws InterruptedException {
+    public void deletePlayList() {
         login("chernyakma75@gmail.com", "te$t$tudent");
+        WebElement playlist = driver.findElement(By.cssSelector("[class='playlist playlist']>a"));
+        playlist.click();
+        WebElement deletePlayListButton = driver.findElement(By.cssSelector("[title='Delete this playlist']"));
+        deletePlayListButton.click();
+       // WebElement ok = driver.findElement(By.cssSelector("[class='ok']"));
+       // ok.click();
+        WebElement SuccessShow = driver.findElement(By.cssSelector(".success.show"));
+        Assert.assertTrue(SuccessShow.isDisplayed());
 
-        WebElement SearchField= driver.findElement(By.cssSelector("[type='search']"));
-        SearchField.click();
-        SearchField.clear();
-        SearchField.sendKeys("Dark days");
 
-        WebElement viewAllButton = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
-        viewAllButton.click();
 
-        WebElement FirstSongButton = driver.findElement(By.cssSelector("#songResultsWrapper .items > tr:nth-of-type(1) > .title"));
-        FirstSongButton.click();
-
-        WebElement addToButton = driver.findElement(By.cssSelector(".btn-add-to"));
-        addToButton.click();
-
-        WebElement ChoosePlaylistButton = driver.findElement(By.xpath("//*[@id='songResultsWrapper']//li[contains(text(),'P1')] "));
-        ChoosePlaylistButton.click();
-
-        WebElement Notification = driver.findElement(By.cssSelector(".success.show"));
-        Assert.assertTrue(Notification.isDisplayed());}
     }
+}
